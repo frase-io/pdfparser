6b7e7f78deda3cdb654fe4a7ba3cb1fa6201c82b
Bryant Moscon <bmoscon@gmail.com>
2018-02-04 20:57:26 -0500

    tested version of image extraction

--- pdfparse/pdfparse.py
+++ pdfparse/pdfparse.py
@@ -1,15 +1,36 @@
+import subprocess
+import random
+import string
+import os
+
 import pdftotext as ptt
+
 
 class PDFParse(object):
     def __init__(self, file_name):
         self.file = file_name
     
     def extract_text(self):
+        """
+        returns unformatted text as a string
+        """
         with open(self.file, "rb") as f:
             pdf = ptt.PDF(f)
 
             return "".join(pdf)
     
     def extract_images(self):
-        pass
+        """
+        returns list of paths to the images
+        extracted from the pdf
+        """
+        prefix = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+        subprocess.call(['pdfimages', '-all', self.file, '/tmp/' + prefix])
+        
+        ret = []
+        files = os.listdir("/tmp")
+        for f in files:
+            if prefix in f:
+                ret.append(os.path.join("/tmp", f))
 
+        return sorted(ret)
